468d99ace88e312ba40a39a2bbeffe168aeaac9b
supaporn085 <may0936107239@gmail.com>
2017-11-19 14:51:48 +0700

    Testing upload to github

---
+++ src/oop_lab/s357411110085/rmutsv/com/HelloWorld.java
@@ -0,0 +1,13 @@
+package oop_lab.s357411110085.rmutsv.com;
+
+public class HelloWorld {
+
+//     create main_method
+    public static void main(String[] arge){
+
+        System.out.println("Hello World,I'am may");
+    }//main
+
+
+
+}

71e2c5154e1ca90d1af2265dc93c023a16538c25
supaporn085 <may0936107239@gmail.com>
2017-11-21 11:21:07 +0700

    Test Variable

--- src/oop_lab/s357411110085/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s357411110085/rmutsv/com/HelloWorld.java
@@ -2,18 +2,14 @@ package oop_lab.s357411110085.rmutsv.com;
 
 public class HelloWorld {
 
-//     create main_method
-    public static void main(String[] arge){
+//     create main method
+    public static void main(String[] args){
 
         System.out.println("Hello World,I'am may");
-        System.out.println("Puriwat Lertkrai");
+        System.out.println("Supaporn NiTiprapan");
         System.out.println("Information System, MT:RMUTSV");
 
 
+}//main
 
-
-    }//main
-
-
-
-}
+}//class
---
+++ src/oop_lab2/TestVariable.java
@@ -0,0 +1,61 @@
+package oop_lab2;
+
+public class TestVariable {
+
+
+    public static void main(String[] args) {
+//      integer
+        int i;
+        i=10;
+        System.out.println("i = "+i);
+        int j = 20;
+        System.out.println("j ="+j);
+        int x = i + j;
+        System.out.println("x="+x);
+//          Double
+        double v = 10.10;
+        System.out.println("v ="+v);
+        x = (int) v;
+        System.out.println("x ="+x);
+//          Char
+        char c = 'T';
+        System.out.println("c =" + c);
+//          String
+        String s = "Hello, World";
+        System.out.println(s);
+        String name = "I'm may";
+        System.out.println(s+" "+name);
+
+        String msg = s.concat(name);
+        System.out.println(msg);
+        System.out.println(msg.length());
+        System.out.println(msg.toUpperCase());
+        System.out.println(msg.toLowerCase());
+
+        int number = 10;
+        int m = ++number;
+        System.out.println(number);
+        System.out.println(m);
+
+
+        m += 1; //m = m+1; m=12
+        System.out.println(m);
+        m += 10;  // m=m+10; m=22
+        System.out.println(m);
+        m -= 5;   // m = m-5; m=17
+        m *= 10;  // m = m*10; m=170
+        System.out.println(m);
+
+//          Boolean
+        boolean b = true;
+        boolean u = false;
+        System.out.println(b);
+        b = false;
+        System.out.println(b);
+
+
+
+    }//main
+
+
+}// class

35ea55ed45f0a4fc986724ee38d36f40baf6c930
supaporn085 <may0936107239@gmail.com>
2017-12-19 11:24:36 +0700

    Test Array

---
+++ src/oop_lab5/TestArry.java
@@ -0,0 +1,35 @@
+package oop_lab5;
+
+public class TestArry {
+    public static void main(String[] args) {
+        int []number;
+        number = new int[5];
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+        System.out.println(number[2]);
+        System.out.println(number[3]);
+        System.out.println(number[4]);
+        number[3] = 100;
+        number[1] = 50;
+        number[2] = number[1] + number[3];
+        number[2+2] = number[1] * 3; //150
+//        System.out.println(number[0]);
+//        System.out.println(number[1]);
+//        System.out.println(number[2]);
+//        System.out.println(number[3]);
+//        System.out.println(number[4]);
+//           for
+        System.out.println("Data in array: ");
+        for (int i=0;i<number.length;i++){
+            System.out.println ("number["+i+"]:"+number[i]+" ");
+
+        }//for
+
+        for (int val:number){
+            System.out.print(val+" ");
+        }
+//        int num[] = new int[500];
+//        num[-1] = 10;
+
+    }//main
+}//calss

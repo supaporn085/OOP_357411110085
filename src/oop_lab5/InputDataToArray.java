02d144d499626b4baa7538eb1d65ee8e1df469e3
supaporn085 <may0936107239@gmail.com>
2017-12-19 12:03:31 +0700

    Test DataToArray

--- src/oop_lab5/InputDataToArray.java
+++ src/oop_lab5/InputDataToArray.java
@@ -14,16 +14,22 @@ public class InputDataToArray {
         int num[] = new int[MAX];
         num = inputData(num);
         ShowData(num);
+        summation(num);
+
     }//main
 
     private static void summation(int[] num){
         int total = 0;
-        for ()
+        for (int i=0;i<num.length;i++){
+            total += num[i];
+        }
+        System.out.println("\nThe summation of " + "value in array is: "+total);
+        System.out.println("The average value " + "in array is: "+total/MAX);
     }
 
     private static void ShowData(int[] num) {
         System.out.println("Data in array: ");
-        for (int val:num)
+        for (int val: num)
             System.out.print(val+" ");
     }
 

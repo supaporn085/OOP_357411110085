e1d387d35b96fc9e76c080e593aa489f6e9594f3
supaporn085 <may0936107239@gmail.com>
2017-12-27 18:00:24 +0700

    Stringcompare

---
+++ src/oop_lab6/StringCompare.java
@@ -0,0 +1,25 @@
+package oop_lab6;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.CompareGenerator;
+
+public class StringCompare {
+    public static void main(String[] args) {
+//        Compare String
+        String str1 = "Hello";
+        String str2 = "Hellooo";
+//          type 1 (==) true,false
+        if (str1==str2) System.out.println("true");
+        else System.out.println("false");
+        //type 2 (equals() method)
+        if (str1.equals(str2))System.out.println("true");
+        else System.out.println("false");
+        //type 3 (compareTo() method)
+        //-,+,0
+        if (str1.compareTo(str2)==0)
+            System.out.println("true");
+        else if (str1.compareTo(str2)>=1)
+            System.out.println("str 2 more than str1");
+        else
+            System.out.println("str1 less than str2");
+    }//main
+}//class

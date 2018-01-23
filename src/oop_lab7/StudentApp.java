7b81feedab2607d993ce081c6654668c0751d6aa
supaporn085 <may0936107239@gmail.com>
2018-01-23 09:17:45 +0700

    Class and ob

---
+++ src/oop_lab7/StudentApp.java
@@ -0,0 +1,12 @@
+package oop_lab7;
+
+public class StudentApp {
+    public static void main(String[] args) {
+        //create objects es Stuent
+        Studant string1 = new  Studant();
+
+
+
+
+    }m
+}

96c32ccb07f6b6060d28c4454da77179e5712bdc
LAB_COM-MT_1 <may0936107239@gmail.com>
2018-01-30 09:46:20 +0700

    Person

---
+++ src/oop_lab8/Personlnfo.java
@@ -0,0 +1,12 @@
+package oop_lab8;
+
+public class Personlnfo {
+    public static void main(String[] args) {
+        Student student = new Student( "357411110085",
+                "may supaporn",
+                "Male",22,
+                "357411110085",
+                "IS :Management Technolgy");
+        System.out.println(student.toString());
+    }
+}

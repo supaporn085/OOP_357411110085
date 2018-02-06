719976e959da95b9bf58a0f92d77a3349fb12235
supaporn085 <may0936107239@gmail.com>
2018-02-06 11:03:39 +0700

    myPersonApp

--- src/oop_lab9/PersonApp.java
+++ src/oop_lab9/PersonApp.java
@@ -9,5 +9,8 @@ public class PersonApp {
         Person person = new Person("1809900651821","maySupaporm",address,job);
         System.out.println(person.toString());
 
+        person.getJob().setSalary(25000);
+        System.out.println(person.getJob().getSalary());
+        System.out.println(person.getAddress().getPostCode());
     }
 }
